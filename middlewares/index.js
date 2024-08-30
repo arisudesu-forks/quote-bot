da1d118b9a6dc2675f98351c3a42b46a0dd78357
Arisu Desu <arisudesu@yandex.ru>
2024-08-30 15:17:40 +0300

    Remove unused middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,7 @@
 const onlyGroup = require('./only-group')
 const onlyAdmin = require('./only-admin')
-const scenes = require('./scenes')
 
 module.exports = {
-  onlyPm: require('./only-pm'),
   onlyGroup,
-  onlyAdmin,
-  scenes
+  onlyAdmin
 }
